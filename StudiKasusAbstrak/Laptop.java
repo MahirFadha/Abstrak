8414323be6e7a87020ad70e69598949d23649641
MahirFadha <159881303+MahirFadha@users.noreply.github.com>
2024-09-03 14:55:12 +0700

    Add files via upload

---
+++ StudiKasusAbstrak/Laptop.java
@@ -0,0 +1,8 @@
+package StudiKasusAbstrak;
+
+public class Laptop extends Checkout{
+    public void cekbarang(int barang){
+        paket(barang);
+        otw();
+    }
+}
---
+++ StudiKasusAbstrak/Test.java
@@ -0,0 +1,11 @@
+package StudiKasusAbstrak;
+
+public class Test {
+    public static void main(String[] args) {
+        Checkout A = new Laptop();
+        A.cekbarang(1);
+
+        Paku P = new Paku();
+        P.cekbarang(102);
+    }
+}
